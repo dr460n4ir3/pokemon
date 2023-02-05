9a2ac1ab1a432efc7ed38b3f06c1d88bcf8dd208
dr460n4ir3 <token.b@gmail.com>
2023-02-04 22:53:26 -0500

    added a tilemap and collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #![allow(clippy::redundant_field_names)]
-use bevy::{prelude::*, render::{camera::ScalingMode}};
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 // This draws the screen
-pub const  CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
 
 // This will point the main function to the player plugin and the player.rs file
+mod ascii;
+mod debug;
 mod player;
+mod tilemap;
+
+use ascii::AsciiPlugin;
+use debug::DebugPlugin;
 use player::PlayerPlugin;
+use tilemap::TileMapPlugin;
 
 // This is the main function also hold systems
 fn main() {
@@ -22,10 +30,12 @@ fn main() {
             resizable: false,
             ..Default::default()
         })
-        .add_startup_system(spawn_camera)
-        .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
         .add_plugins(DefaultPlugins)
-        .add_plugin(PlayerPlugin)
+        .add_startup_system(spawn_camera)
+        .add_plugin(PlayerPlugin)        
+        .add_plugin(AsciiPlugin)
+        .add_plugin(DebugPlugin)
+        .add_plugin(TileMapPlugin)
         .run();
 }
 
@@ -43,26 +53,3 @@ fn spawn_camera(mut commands: Commands) {
 
     commands.spawn_bundle(camera);
 }
-
-struct AsciiSheet(Handle<TextureAtlas>);
-
-fn load_ascii(
-    mut commands: Commands,
-    assets: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>) {
-        let image = assets.load("Ascii.png");
-        let atlas = TextureAtlas::from_grid_with_padding(
-            image,
-            Vec2::splat(9.0),
-            16,
-            16,
-            Vec2::splat(2.0)
-        );
-
-        let atlas_handle
-            = texture_atlases.add(atlas);
-
-        commands.insert_resource(AsciiSheet(atlas_handle));
-}
-
-//
--- src/player.rs
+++ src/player.rs
@@ -1,93 +1,152 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::collide_aabb::collide};
+use bevy_inspector_egui::Inspectable;
 
-use crate::AsciiSheet;
+use crate::{
+    ascii::{spawn_ascii_sprite, AsciiSheet},
+    tilemap::TileCollider,
+    TILE_SIZE,
+};
 
 // use bevy::input::gamepad::Gamepad; // this is for gamepad functionality
 
 pub struct PlayerPlugin;
 
-#[derive(Component)]
+#[derive(Component, Inspectable)]
 pub struct Player {
     pub speed: f32, // this is the speed of the player
 }
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player);
-        app.add_system(player_controller);
+        app.add_startup_system(spawn_player)
+        .add_system(camara_follow.after("movement"))
+        .add_system(player_controller.label("movement"));
     }
+}
+
+// This function Player movement camera
+fn camara_follow(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (Without<Player>, With<Camera>)>,
+) {
+    let player_transform = player_query.single();
+    let mut camera_transform = camera_query.single_mut();
+
+    camera_transform.translation.x = player_transform.translation.x;
+    camera_transform.translation.y = player_transform.translation.y;
 }
 
 // This is the player movement controller
 fn player_controller(
     mut player_query: Query<(&Player, &mut Transform)>,
+    wall_query: Query<&Transform, (With<TileCollider>, Without<Player>)>, // this tells the game if the player is colliding with a wall
     keyboard_input: Res<Input<KeyCode>>, // this tells the game if the key is pressed
     // gamepad: Res<Gamepad>, // this tells the game if the gamepad is pressed
     time: Res<Time>, // this tells the game how much time has passed
 ) {
     let (player, mut transform) = player_query.single_mut(); // this tells the game to only have one player
-    // change the following to work with multiple players: let (player, mut transform) = player_query.single_mut().unwrap();
-    // let mut direction = Vec3::ZERO;
 
-    if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-        transform.translation.y += player.speed * time.delta_seconds();
+
+    // NOTE: Only use this if you want to use the arrow keys and WASD keys at the same time PRODUCTION only
+    // if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+    //     transform.translation.y += player.speed * time.delta_seconds();
+    // }
+
+    // if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+    //     transform.translation.y -= player.speed * time.delta_seconds();
+    // }
+
+    // if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+    //     transform.translation.x -= player.speed * time.delta_seconds();
+    // }
+
+    // if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+    //     transform.translation.x += player.speed * time.delta_seconds();
+    // }
+
+    // NOTE: its best to use this for Development so arrow keys can be used for testing/editing
+    let mut y_delta = 0.0;
+    if keyboard_input.pressed(KeyCode::W) {
+        y_delta += player.speed * TILE_SIZE * time.delta_seconds();
     }
 
-    if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-        transform.translation.y -= player.speed * time.delta_seconds();
+    if keyboard_input.pressed(KeyCode::S) {
+        y_delta -= player.speed * TILE_SIZE * time.delta_seconds();
     }
 
-    if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-        transform.translation.x -= player.speed * time.delta_seconds();
+    let mut x_delta = 0.0;
+    if keyboard_input.pressed(KeyCode::A) {
+        x_delta -= player.speed * TILE_SIZE * time.delta_seconds();
     }
 
-    if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-        transform.translation.x += player.speed * time.delta_seconds();
+    if keyboard_input.pressed(KeyCode::D) {
+        x_delta += player.speed * TILE_SIZE * time.delta_seconds();
+    }
+    
+
+    // transform.translation += Vec3::new(x_delta, y_delta, 0.0);
+    let target = transform.translation + Vec3::new(x_delta, 0.0, 0.0);
+    if collision_check(target, &wall_query) {
+        transform.translation = target;
+    }
+
+    let target = transform.translation + Vec3::new(0.0, y_delta, 0.0);
+    if collision_check(target, &wall_query) {
+        transform.translation = target;
     }
 
     // NOTE: need to implement gamepad functionality
+}
 
-
+// This function controls collision with walls
+fn collision_check(
+    target_player_pos: Vec3,
+    wall_query: &Query<&Transform, (With<TileCollider>, Without<Player>)>,
+) -> bool {
+    for wall_transform in wall_query.iter() {
+        let collision = collide(
+            target_player_pos,
+            Vec2::splat(TILE_SIZE * 0.9),
+            wall_transform.translation,
+            Vec2::splat(TILE_SIZE),
+        );
+        if collision.is_some() {
+            return false;
+        }
+    }
+    true
 }
 
 // This add the player sprite to the game
 fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
-    let mut sprite = TextureAtlasSprite::new(1);
-    sprite.color = Color::rgb(1.0, 6.5, 0.0);
-    sprite.custom_size = Some(Vec2::splat(1.0));
+    let player = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        1,
+        Color::rgb(1.0, 6.5, 0.0),
+        Vec3::new(0.810, -0.636, 900.0), //this is where the player will spawn
+    );
 
-    let player = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: sprite,
-            texture_atlas: ascii.0.clone(),
-            transform: Transform { 
-                translation: Vec3::new(0.0, 0.0, 900.0), 
-                ..Default::default()
-            },
-            ..Default::default()
-        })
+    commands
+        .entity(player)
         .insert(Name::new("Player"))
         .insert(Player {
             speed: 4.0, // this is the speed of the player
         })
         .id();
 
-    let mut background_sprite = TextureAtlasSprite::new(0);
-    background_sprite.color = Color::rgb(0.5, 0.5, 0.5);
-    background_sprite.custom_size = Some(Vec2::splat(1.0));
+    let background = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        0,
+        Color::rgb(0.5, 0.5, 0.5),
+        Vec3::new(0.0, 0.0, -1.0),
+    );
 
-    let background = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: background_sprite,
-            texture_atlas: ascii.0.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, -1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
+    commands
+        .entity(background)
         .insert(Name::new("Background"))
-        .id();
+        .id(); // this give back the entity after it has been created
 
     commands.entity(player).push_children(&[background]);
 }
